86f74d720468a6e97521e7df64f4f4a25480ac6e
Hammad Ali <hammadaali02@gmail.com>
2024-09-06 20:56:12 +0500

    updated

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,6 +8,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Hive X Flutter',
       home: HomePage(),
     );
