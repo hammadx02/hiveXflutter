386bb730f4820b036158f6e92f96561ff55b6e38
Hammad Ali <hammadaali02@gmail.com>
2024-09-06 20:57:24 +0500

    updated

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,31 +8,41 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Hive X Flutter'),
+        title: const Text(
+          'Hive X Flutter',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Colors.blue,
       ),
-      body: Row(
-        children: [
-          MaterialButton(
-            onPressed: () {},
-            color: Colors.blueAccent,
-            child: const Text('Write'),
-          ),
-          MaterialButton(
-            onPressed: () {},
-            color: Colors.greenAccent,
-            child: const Text('Read'),
-          ),
-          MaterialButton(
-            onPressed: () {},
-            color: Colors.redAccent,
-            child: const Text('Delete'),
-          ),
-        ],
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            MaterialButton(
+              onPressed: () {},
+              color: Colors.blueAccent,
+              child: const Text('Write'),
+            ),
+            MaterialButton(
+              onPressed: () {},
+              color: Colors.greenAccent,
+              child: const Text('Read'),
+            ),
+            MaterialButton(
+              onPressed: () {},
+              color: Colors.redAccent,
+              child: const Text('Delete'),
+            ),
+          ],
+        ),
       ),
     );
   }
