7acef02c238394be4bd06fd826bd556361160444
Hammad Ali <hammadaali02@gmail.com>
2024-09-06 20:49:13 +0500

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hivexflutter/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,9 +11,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Hive X Flutter',
-      home: const HomePage(),
+      home: HomePage(),
     );
   }
 }
