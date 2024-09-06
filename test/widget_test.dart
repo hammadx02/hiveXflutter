80877ecb120274b6c616efa25ad3a520c2434400
Hammad Ali <hammadaali02@gmail.com>
2024-09-06 20:48:51 +0500

    updated

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:hive/main.dart';
+import 'package:hivexflutter/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
